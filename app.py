d48e0f76f2c37bfc6b3d029c8edf39a01a19399a
Luke Barker <BarkerLJ@cardiff.ac.uk>
2018-05-20 14:32:52 +0100

    removed parser
     * now located in controllers.py

--- app.py
+++ app.py
@@ -3,11 +3,3 @@ from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
-
-parser = reqparse.RequestParser()
-
-########### ARGUMENTS ###########
-
-
-
-########### RESOURCES ###########
