9b7697728264d241a455dfda080b2f368fa31597
Luke Barker <BarkerLJ@cardiff.ac.uk>
2018-05-21 02:53:54 +0100

    new endpoint
     * algorithms

--- app.py
+++ app.py
@@ -1,5 +1,16 @@
 from flask import Flask, redirect, json
 from flask_restful import reqparse, abort, Api, Resource
 
+from controllers import AlgorithmController
+
 app = Flask(__name__)
 api = Api(app)
+
+################# CONTROLLERS #################
+
+api.add_resource(AlgorithmController, '/algorithms/<algorithmname>')
+
+############# END OF CONTROLLERS ##############
+
+if __name__ == '__main__':
+    app.run(debug=True)
