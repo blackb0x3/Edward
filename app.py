bbc1f79ac0a18aa133c0462d1de65bea8ade186e
Luke Barker <BarkerLJ@cardiff.ac.uk>
2018-10-27 15:46:34 +0100

    Import GraphController

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, json
 from flask_restful import reqparse, abort, Api, Resource
 
-from controllers import AlgorithmController, AlgorithmListController
+from controllers import AlgorithmController, AlgorithmListController, GraphController
 
 app = Flask(__name__)
 api = Api(app)
