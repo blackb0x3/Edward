5fa818ad24ce852c370dc8498972e0b6251a8d38
Luke Barker <BarkerLJ@cardiff.ac.uk>
2018-11-11 23:53:21 +0000

    Add list of algorithm names
     * required for CompareChart

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__)) # This is your Project Root
+
+algorithm_names = {
+    "insertion-sort":          "Insertion Sort",
+    "selection-sort":          "Selection Sort",
+    "optimised-bubble-sort":   "Optimised Bubble Sort",
+    "traditional-bubble-sort": "Traditional Bubble Sort",
+    "recursive-quick-sort":    "Recursive Quick Sort",
+    "iterative-quick-sort":    "Iterative Quick Sort",
+    "top-down-merge-sort":     "Top Down Merge Sort",
+    "bottom-up-merge-sort":    "Bottom Up Merge Sort",
+    "heap-sort":               "Heap Sort",
+    "shell-sort":              "Shell Sort",
+    "counting-sort":           "Counting Sort",
+    "bucket-sort":             "Bucket Sort",
+}
