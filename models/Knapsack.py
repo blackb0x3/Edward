793c164a11e6427058cd545e911968ccf77558c1
Luke Barker <BarkerLJ@cardiff.ac.uk>
2019-02-19 14:20:32 +0000

    Add methods for removing and updating knapsacks

--- models/Knapsack.py
+++ models/Knapsack.py
@@ -17,6 +17,8 @@ class Knapsack:
         new_item = KnapsackItem(cost, val)
         pointer = 0
 
+        # we want to insert items whilst keeping the order of the list
+        # increases search efficiency
         while pointer <= len(self.items):
             if new_item.value < self.items[pointer].value:
                 self.items.insert(pointer, new_item)
@@ -28,4 +30,19 @@ class Knapsack:
 
             pointer += 1
 
-        #self.items.append(item)
+    def remove_item(self, pointer=False, cost=False, value=False):
+        if pointer is not False:
+            return self.items.pop(pointer) if pointer is not False and pointer < len(self.items) else False
+
+        elif pointer is False
+                and cost is not False
+                and value is not False:
+            for ksi in self.items[:]:
+                if ksi.cost == cost and ksi.value == value:
+                    return self.items.remove(ksi)
+
+        else:
+            raise ValueError("You must supply either a pointer or a cost and value.")
+
+    def update_item(self, pointer, cost, value):
+        return self.items[pointer] = KnapsackItem(cost, value)
