1db5051d69dc9c47642b80e9cfe7221a0a785559
Luke Barker <BarkerLJ@cardiff.ac.uk>
2019-05-17 18:04:50 +0100

    Modify folder locations and add app controller
     * allows flask to render the react app without yarn
     * main.js script needs to be removed though...

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
-from flask import Flask, redirect, json
+from flask import Flask, redirect, json, render_template
 from flask_restful import reqparse, abort, Api, Resource
 
 from controllers import AlgorithmController, AlgorithmListController, GraphController
 
-app = Flask(__name__, static_folder="./static/dist", template_folder="./static/views")
+app = Flask(__name__, template_folder="./static/dist")
 api = Api(app)
 
-################# CONTROLLERS #################
+################# API CONTROLLERS #################
 
-api.add_resource(AlgorithmListController, '/algorithms')
-api.add_resource(AlgorithmController, '/algorithms/<algorithmname>')
-api.add_resource(GraphController, '/algorithms/graphs/<graphid>')
+api.add_resource(AlgorithmListController, '/api/algorithms')
+api.add_resource(AlgorithmController, '/api/algorithms/<algorithmname>')
+api.add_resource(GraphController, '/api/algorithms/graphs/<graphid>')
 
-############# END OF CONTROLLERS ##############
+############# END OF API CONTROLLERS ##############
+
+
+
+
+##################### APP ENDPOINT ####################
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+################# END OF APP ENDPOINT #################
 
 if __name__ == '__main__':
     app.run(debug=True)
