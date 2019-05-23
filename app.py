815e33b47ec0de6236f94555e127265724ddf0bc
Luke Barker <BarkerLJ@cardiff.ac.uk>
2019-05-23 18:36:09 +0100

    Modify react build command
     * works with windows + linux

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_restful import reqparse, abort, Api, Resource
 
 from controllers import AlgorithmController, AlgorithmListController, GraphController
 
-REACT_BUILD_COMMAND = "cd .\static && yarn run build"
+REACT_BUILD_COMMAND = "cd static && yarn run build"
 
 app = Flask(__name__, template_folder="./static/dist")
 api = Api(app)
