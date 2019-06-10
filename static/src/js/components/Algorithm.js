a18167ddc097fe772303ad7d56a6c8d98aeec268
Luke Barker <BarkerLJ@cardiff.ac.uk>
2019-06-10 08:26:16 +0100

    input output types, commenting

--- static/src/js/components/Algorithm.js
+++ static/src/js/components/Algorithm.js
@@ -16,6 +16,7 @@ import {
 import axios from "axios";
 import { List, Graph } from "./AlgorithmInputs/index";
 import AlgorithmOutput from "./AlgorithmOutput";
+import INPUT_OUTPUT_TYPES from "../config";
 
 export default class Algorithm extends Component {
   constructor(props) {
@@ -25,6 +26,8 @@ export default class Algorithm extends Component {
     this.state = {
       algorithmOutput : null,
          algorithmKey : props.algorithmKey,
+         inputType    : props.inputType,
+         outputType   : props.outputType,
          name         : "",
          description  : "",
          steps        : [],
@@ -53,6 +56,10 @@ export default class Algorithm extends Component {
       .catch(err => alert(err));
   }
 
+  /**
+   * Sets the user's desired action for the algorithm.
+   * @param {*} e The event object.
+   */
   setAlgorithmAction(e) {
     e.preventDefault();
 
@@ -63,6 +70,10 @@ export default class Algorithm extends Component {
     }, () => console.log(this.state));
   }
 
+  /**
+   * Callback function which activates the chosen algorithm via the Python API, and renders the results
+   * @param {*} postData The post body to send to the Python API.
+   */
   onPostDataReceived(postData) {
     axios.post(`/api/algorithms/${this.state.algorithmKey}`, postData)
       .then(resp => {
@@ -84,20 +95,38 @@ export default class Algorithm extends Component {
     // input type (string, list, dict, graph etc.) can change based on the algorithm
     // e.g. binary search needs a list, but djikstra requires a graph
     switch (this.props.inputType) {
-      case "list":
-        algorithmFormInput = <List  algorithmKey={this.state.algorithmKey} onPostDataReceived={this.onPostDataReceived} action={this.state.action} />;
+      case INPUT_OUTPUT_TYPES.LIST:
+        algorithmFormInput = <List algorithmKey={this.state.algorithmKey} onPostDataReceived={this.onPostDataReceived} action={this.state.action} />;
         break;
 
-      case "graph":
+      case INPUT_OUTPUT_TYPES.GRAPH:
         algorithmFormInput = <Graph algorithmKey={this.state.algorithmKey} onPostDataReceived={this.onPostDataReceived} action={this.state.action} />;
         break;
 
       // TODO
-      case "string":
+      case INPUT_OUTPUT_TYPES.STRING:
         algorithmFormInput = <div>Hello World!</div>;
+        break;
 
+      // TODO
+      case INPUT_OUTPUT_TYPES.NUMBER:
+        algorithmFormInput = <div>Hello World!</div>;
+        break;
+
+      // TODO
+      case INPUT_OUTPUT_TYPES.DICT:
+        algorithmFormInput = <div>Hello World!</div>;
+        break;
+
+      // TODO
+      case INPUT_OUTPUT_TYPES.BOOLEAN:
+        algorithmFormInput = <div>Hello World!</div>;
+        break;
+
+      // TODO
+      case INPUT_OUTPUT_TYPES.CUSTOM:
       default:
-        algorithmFormInput = <List  algorithmKey={this.state.algorithmKey} onPostDataReceived={this.onPostDataReceived} action={this.state.action} />;
+        algorithmFormInput = <List algorithmKey={this.state.algorithmKey} onPostDataReceived={this.onPostDataReceived} action={this.state.action} />;
         break;
     }
 
