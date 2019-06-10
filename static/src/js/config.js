c75a1fc8b4b9eefeb1f67bfa3bbf0140d999d62a
Luke Barker <BarkerLJ@cardiff.ac.uk>
2019-06-10 08:30:10 +0100

    Add config file for globals
     * currently contains input output types

---
+++ static/src/js/config.js
@@ -0,0 +1,11 @@
+const INPUT_OUTPUT_TYPES = {
+     LIST : "list",
+    GRAPH : "graph",
+   STRING : "string",
+  BOOLEAN : "boolean",
+     DICT : "dict",
+   NUMBER : "number",
+   CUSTOM : "object"
+};
+
+export default INPUT_OUTPUT_TYPES;
