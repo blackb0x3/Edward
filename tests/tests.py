efe56a64726a232c02705321152db56b1530b44f
Luke Barker <developer.luke.barker3@outlook.com>
2019-09-04 23:52:36 +0100

    Create method outlines for POST tests

--- tests/tests.py
+++ tests/tests.py
@@ -49,3 +49,75 @@ class AlgorithmControllerTests(unittest.TestCase):
         # when performing a GET to /api/algorithms/<algorithm_key>
         # then expect HTTP 404 response
         pass
+
+    def test_post_not_implemented(self):
+        # given valid POST request
+        # when performing a POST to /api/algorithms/<algorithm_key>
+        # then expect HTTP 501
+        pass
+
+    def test_post_no_action(self):
+        # given POST request with no 'action' entry in JSON
+        # when performing a POST to /api/algorithms/<algorithm_key>
+        # then expect HTTP 400
+        pass
+
+    def test_pot_invalid_action(self):
+        # given POST request with invalid 'action' entry in JSON
+        # when performing a POST to /api/algorithms/<algorithm_key>
+        # then expect HTTP 400
+        pass
+
+    def test_post_invalid_min_size(self):
+        # given POST request with invalid 'min_size' entry in 'options' JSON
+        # when performing a POST to /api/algorithms/<algorithm_key>
+        # then expect HTTP 400
+        pass
+
+    def test_post_invalid_max_size(self):
+        # given POST request with invalid 'max_size' entry in 'options' JSON
+        # when performing a POST to /api/algorithms/<algorithm_key>
+        # then expect HTTP 400
+        pass
+
+    def test_post_invalid_jump(self):
+        # given POST request with invalid 'jump' entry in 'options' JSON
+        # when performing a POST to /api/algorithms/<algorithm_key>
+        # then expect HTTP 400
+        pass
+
+    def test_post_invalid_repeats(self):
+        # given POST request with invalid 'repeats' entry in 'options' JSON
+        # when performing a POST to /api/algorithms/<algorithm_key>
+        # then expect HTTP 400
+        pass
+
+    def test_post_algorithm_type(self):
+        # given POST request comparing two algorithms which solve different problems
+        # when performing a POST to /api/algorithms/<algorithm_key>
+        # then expect HTTP 400
+        pass
+
+    def test_post_max_min_size(self):
+        # given POST request where max_size is lower than min_size
+        # when performing a POST to /api/algorithms/<algorithm_key>
+        # then expect HTTP 400
+        pass
+
+    def test_post_valid_request_run(self):
+        # given valid post body with 'run' action
+        # when performing a POST to /api/algorithms/<algorithm_key>
+        # then expect HTTP 200 OK - experiment result
+        pass
+
+    def test_post_valid_request_test(self):
+        # given valid post body with 'test' action
+        # when performing a POST to /api/algorithms/<algorithm_key>
+        # then expect HTTP 200 OK - experiment result
+        pass
+
+    def test_post_valid_request_compare(self):
+        # given valid post body with 'compare' action
+        # when performing a POST to /api/algorithms/<algorithm_key>
+        # then expect HTTP 200 OK - experiment result
+        pass
