f20142966bf799f14f237995faea5403796d3c50
Luke Barker <BarkerLJ@cardiff.ac.uk>
2019-09-21 09:58:08 +0100

    Finish tests for algorithm type and algorithm list

--- tests/tests.py
+++ tests/tests.py
@@ -2,48 +2,79 @@ import requests, unittest
 
 BASE_URL = "http://localhost:5000"
 
+# when writing tests, assert the structure of the responses, rather than actual data
+
+
 class AlgorithmListControllerTests(unittest.TestCase):
     def test_get(self):
         # given no parameters
         # when performing a GET to /api/algorithms
-        response = requests.get(f"{BASE_URL}/api/algorithms").json()
+        response_with_http = requests.get(f"{BASE_URL}/api/algorithms")
+        response = response_with_http.json()
 
         # then expect HTTP 200 OK - list of available algorithm keys
-        key = "available_algorithms"
-
-        # check structure of response, rather than actual data
-        self.assertTrue(key in response.keys())
-        self.assertTrue(type(response[key]) is list)
-        self.assertTrue(all(type(entry).__name__ == "str" for entry in response[key]))
+        self.assertTrue(response_with_http.ok())
+        self.assertTrue(response_with_http.status_code == 200)
+        correct_key = "available_algorithms"
+        self.assertTrue(correct_key in response.keys())
+        self.assertTrue(type(response[correct_key]) is list)
+        self.assertTrue(len(response[correct_key]) > 0)
+        self.assertTrue(all([type(entry).__name__ == "str" for entry in response[correct_key]]))
 
 
 class AlgorithmTypesControllerTests(unittest.TestCase):
     def test_get_valid_arg(self):
         # given valid algorithm type key
+        algorithm_type_to_use = "sorting"
+
         # when performing a GET to /api/algorithmType/<algorithm_type>
+        response_with_http = requests.get(f"{BASE_URL}/api/algorithmType/{algorithm_type_to_use}")
+        response = response_with_http.json()
+
         # then expect list of available algorithm keys of that type
-        pass
+        self.assertTrue(response_with_http.ok())
+        self.assertTrue(response_with_http.status_code == 200)
+        self.assertTrue(type(response) is dict)
+        self.assertTrue(len(response.keys()) > 0)
+        self.assertTrue(all([type(key).__name__ == "str" for key in response.keys()]))
+        self.assertTrue(all([type(val).__name__ == "str" for val in response.values()]))
 
     def test_get_no_args(self):
         # given no args
-        # when performing a GET to /api/algorithmType/<algorithm_type>
-        # then expect HTTP 400 response
-        pass
+        # when performing a GET to /api/algorithmType
+        response_with_http = requests.get(f"{BASE_URL}/api/algorithmType")
+
+        # then expect HTTP 404 response
+        self.assertFalse(response_with_http.ok())
+        self.assertTrue(response_with_http.status_code == 404)
 
     def test_get_invalid_arg(self):
         # given invalid algorithm type key
+        invalid_key = "abcdefg"
+
         # when performing a GET to /api/algorithmType/<algorithm_type>
-        # then expect HTTP 404 response
-        pass
+        response_with_http = requests.get(f"{BASE_URL}/api/algorithmType")
+        response = response_with_http.json()
+
+        # then expect HTTP 400 response
+        self.assertFalse(response_with_http.ok())
+        self.assertTrue(response_with_http.status_code == 400)
+        self.assertEqual(response["message"], f"Algorithm type '{invalid_key}' does not exist within the API.")
 
 
 class AlgorithmControllerTests(unittest.TestCase):
     def test_get_valid_arg_available(self):
         # given valid algorithm key
+        available_key = "insertion-sort"
+
         # when performing a GET to /api/algorithms/<algorithm_key>
+        response_with_http = requests.get(f"{BASE_URL}/api/algorithms/{available_key}")
+        response = response_with_http.json()
+
         # if algorithm is available
         # then expect HTTP 200 OK - algorithm JSON metadata
-        pass
+        self.assertTrue(response_with_http.ok())
+        self.assertTrue(response_with_http.status_code == 200)
 
     def test_get_valid_arg_unavailable(self):
         # given valid algorithm key
